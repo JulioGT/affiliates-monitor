00e695c4832bd51d900c33a17de4a62f8d606511
Julio Gonzalez <juliojose.gonzalez@gmail.com>
2020-11-09 18:27:42 -0600

    Added Campaign.Model

--- campaigns/tests.py
+++ campaigns/tests.py
@@ -1,16 +1,15 @@
 from django.test import TestCase
+from .models import Campaign
 
 # Create your tests here.
 
-class campaignTest (TestCase):
-  def test_campaign_created(self):
-    """
-    it expects to craate a new campaign successfully
-    """
-    campaign = Campaign.objects.create(name='myCampaign')
-    campaign_name = Campaign.name
 
-    self.assertEqual(str(campaign), campaign_name) 
+class campaignTest(TestCase):
+    def test_campaign_created(self):
+        """
+        it expects to craate a new campaign successfully
+        """
+        campaign = Campaign.objects.create(name="myCampaign")
+        campaign_name = Campaign.name
 
-
- 
+        self.assertEqual(str(campaign), campaign_name)
